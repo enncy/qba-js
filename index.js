8b610e7eedd90457a1a739a69045775449a042a0
enncy <877526278@qq.com>
2023-11-15 14:16:26 +0800

    新增内容替换功能

---
+++ index.js
@@ -0,0 +1,69 @@
+import * as lit from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+const { html, css, LitElement } = lit
+
+export class ReplaceGroup extends LitElement {
+
+    static styles = css`
+        .replace-group-items {
+            margin-right: 12px;
+            border: 1px solid #8d8d8d;
+            border-radius: 4px;
+            font-size: 14px;
+            padding: 4px;
+        }
+ 
+        [id*="arg-1"] {
+            width: 84px;
+        }
+        [id*="arg-2"] {
+            width: 32px;
+        }
+        [id*="arg-3"] {
+            width: 64px;
+        }
+    `
+    static properties = {
+        arg1: { type: String },
+        arg2: { type: String },
+        arg3: { type: String },
+    };
+
+    constructor() {
+        super();
+        this.arg1 = '';
+        this.arg2 = 'g';
+        this.arg3 = '';
+    }
+
+
+    render() {
+        return html`
+        <span class="replace-group-items">
+            <input
+                placeholder="替换表达式"
+                title="支持正则表达式"
+                id="arg-1"
+                value="${this.arg1}"
+                @input="${this.changeArg}"
+            />
+            <input placeholder="flag" id="arg-2"   value="${this.arg2}" @input="${this.changeArg}"/>
+            <input placeholder="代替内容" id="arg-3"  value="${this.arg3}" @input="${this.changeArg}"/>
+            <button @click=${this.removeEl} >x</button>
+        </span>
+        `;
+    }
+
+    changeArg = (e) => {
+        const { id, value } = e.target;
+        const [_, arg] = id.split('-');
+        console.log(arg);
+        this[`arg${arg}`] = value;
+    }
+
+    removeEl = () => {
+        this.remove();
+    }
+}
+
+
+customElements.define('el-replace-group', ReplaceGroup);
